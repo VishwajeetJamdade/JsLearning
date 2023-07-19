92653542105a50346aabe23ff3a5dfce12c2b5f0
VishwajeetJamdade <vishwajeetjamdade@gmail.com>
2023-07-19 16:50:52 +0530

    Variable Declaration and Finding The Which Type of datatype
    Variable Declaration and Finding The Which Type of datatype

---
+++ 02_typeOf.js
@@ -0,0 +1,29 @@
+var bankName;
+console.log("Before Initialization :" +bankName);
+console.log(" Before Type Of Value :"+ typeof bankName );
+bankName="SBI Bank";
+console.log("After Initialization :" +bankName);
+console.log("Data Type is :"+ typeof bankName );
+console.log("-------------------------*******----------------------------------");
+var marks="90%";
+console.log("After Initialization mark is : " + marks);
+console.log("Data Type is :"+ typeof marks );
+
+console.log("-------------------------*******----------------------------------");
+
+var isWorking;
+console.log("Before Initialization :" +isWorking);
+console.log(" Before Data Type Type is :"+ typeof isWorking );
+isWorking="true";
+console.log("After Initialization :" +isWorking);
+console.log(" Data Type is :"+ typeof isWorking );
+console.log("-------------------------*******----------------------------------");
+var totalCount;
+totalCount="Hundred and Seven";
+console.log("After Initialization :" + totalCount);
+console.log("Data Type is :" + typeof totalCount);
+totalCount="107";
+console.log("After Updation :" + typeof totalCount);
+console.log("-------------------------*******----------------------------------");
+
+
