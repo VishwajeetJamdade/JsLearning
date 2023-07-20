e893ba3b683ccc10bfae156d3e1dff678c62c7ca
VishwajeetJamdade <vishwajeetjamdade@gmail.com>
2023-07-20 11:34:33 +0530

    Adding one swapVariable.js
    Swapping using two Or Three Variables

---
+++ 02_swapVariables.js
@@ -0,0 +1,24 @@
+var sweety="Sweety";
+var cutie ="Cutie";
+console.log("       ","Swapping Using Two Values");
+console.log("       ","-------------------------");
+console.log("Before Swapping","  ","sweety :",sweety, " ", "cutie : ",cutie );
+var temp = sweety;
+sweety=cutie;
+cutie=temp;
+console.log("After Swapping","  ","sweety :",sweety,"  ","cutie :",cutie );
+console.log("*************************************************************");
+
+
+console.log("       ","Swapping Using Three Values");
+console.log("       ","-------------------------");
+var num1=100;//200
+var num2=200;//300
+var num3=300;//100
+console.log("Before Swapping","  ","num1 :",num1, " ", "num2 : ",num2, " ", "num3 : ",num3 );
+var temp1=num1;
+num1=num2;
+num2=num3;
+num3=temp1;
+console.log("After Swapping","  ","num1 :",num1, " ", "num2 : ",num2, " ", "num3 : ",num3 );
+console.log("*************************************************************");
