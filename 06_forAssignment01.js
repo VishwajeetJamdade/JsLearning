4e7345c765938fa0ac888a0a189ccd3aeb479f3e
VishwajeetJamdade <vishwajeetjamdade@gmail.com>
2023-07-31 09:39:02 +0530

    Control Flow (for Loop)
    In this assignment using looping statement(For loop ) print tables,reverse no ,even no ,odd no and simply print the numbers.

---
+++ 06_forAssignment01.js
@@ -0,0 +1,33 @@
+console.log(`************************************Task-1 (print no 5 to 15)********************************************************************`);
+for (let index = 5; index <=15; index++) {  //In for Loop 3 things is very important 1 st is initialization 
+                                            //   2 nd is condition and 3 rd is updatation is be increament or decrement
+   console.log(`${index}`);
+    
+}
+console.log(`--------------------------------------------------------------------------------------------------------------`);
+console.log(`************************************Task-2(print nu 50-40 in reverse)********************************************************************`);
+for(let index=50; index>=40; index--)
+{
+    console.log(`${index}`);
+}
+console.log(`************************************Task-3(find first 15 odd no)********************************************************************`);
+for (let index = 1; index <=30; index=index+2) {
+   console.log(`${index}`);
+    
+}
+console.log(`************************************Task-4(find first 10 even no)********************************************************************`);
+for (let index = 0; index <20 ; index=index+2) {
+   console.log(`${index}`);
+}
+console.log(`************************************Task-5(5 table)********************************************************************`);
+for (let index = 5; index <=50; index=index+5) {
+    console.log(`${index}`);
+}
+console.log(`************************************Task-6(10 table)********************************************************************`);
+for (let index = 10; index <=100; index=index+10) {
+   console.log(`${index}`);
+}
+console.log(`************************************Task-7(10 table in reverse order)********************************************************************`);
+for (let index = 100; index >= 10; index=index-10) {
+  console.log(`${index}`);
+}
