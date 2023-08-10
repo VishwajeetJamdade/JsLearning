5b8f8764d0c9c3aaacbe3a90c912fc3301588fdc
VishwajeetJamdade <vishwajeetjamdade@gmail.com>
2023-08-10 16:57:28 +0530

    Modified code 14_jsonAssign
    Adding other ways to do

--- 14_jsonAssign.js
+++ 14_jsonAssign.js
@@ -25,19 +25,39 @@ let array=infoObject.role
             console.log(iterator);
         }
     }
+    // let array=infoObject.role[0];
+// console.log(array);
+
 console.log(`-------------Task-4-----------------------`);
-let fullName=infoObject.name
-let str="";
-for (let index =fullName.length-5 ; index <fullName.length; index++) {
-    const element = fullName[index];
-    str=str+element;
-}
-console.log(str);
+// let fullName=infoObject.name
+// let str="";
+// for (let index =fullName.length-5 ; index <fullName.length; index++) {
+//     const element = fullName[index];
+//     str=str+element;
+// }
+// console.log(str);
+//let fullName=infoObject.name.substring(6)//using substring
+// let fullName=infoObject.name.slice(6)//using slice
+let fullName=infoObject.name.split(" ")
+console.log(fullName[1]);
 console.log(`-------------Task-5-----------------------`);
-let date =infoObject.doj
-let da="";
-      for (let index = date.length-4; index < date.length; index++) {
-          const ele = date[index];
-         da=da+ele; 
-      }
-      console.log(da);
+// let date =infoObject.doj
+// let da="";
+//       for (let index = date.length-4; index < date.length; index++) {
+//           const ele = date[index];
+//          da=da+ele; 
+//       }
+//       console.log(da);
+// let date =infoObject.doj
+// // console.log(typeof date);
+
+// var newDate=new Date(date)
+// let fullYear=newDate.getFullYear
+// console.log(fullYear);
+let date = infoObject.doj;  // Assuming infoObject contains the Date of Joining
+// console.log(typeof date);
+
+var newDate = new Date(date);
+let fullYear = newDate.getFullYear();  // Corrected line: Added parentheses to call the function
+
+console.log(fullYear);
